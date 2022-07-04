69264b450fc6192ed4f1b2473c01e4002b652768
cucpk <98456717+cucpk@users.noreply.github.com>
2022-07-04 16:37:08 +0700

    Bai2

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <LamBai2_1/>    
+    <LamBai2_2/>    
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import LamBai2_1 from './components/LamBai2_1.vue'
+import LamBai2_2 from './components/LamBai2_2.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    LamBai2_1,
+    LamBai2_2
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
